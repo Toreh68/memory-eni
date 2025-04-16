9d18bbb0020ec25e8ade0b57a1eb0a2868c58843
Florent Durand <fdurand68@gmail.com>
2025-04-16 16:50:40 +0200

    feat envoi du mercredi

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 function colorTextGreen(id) {
     document.getElementById(id).style.color = "green"
 }
@@ -5,85 +8,174 @@ function colorTextGreen(id) {
 function colorTextRed(id) {
     document.getElementById(id).style.color = "red"
 }
+document.getElementById('mdp').addEventListener('input', checkConditionsMdp)
+  function checkConditionsMdp(){
+  let mdp = document.getElementById('mdp').value
+ function lengthOk(mdp) {
+    return mdp.length >= 6;
+}
+function number(mdp) {
+    return /[0-9]+/.test(mdp);
+}
+function specialeCar(mdp) {
+    return /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(mdp);
+}
+ let isOk= true
+isOk= lengthOk(mdp) && number(mdp) && specialeCar(mdp) 
 
-document.getElementById('mdp').addEventListener('input', checkPwdLength)
-function checkPwdLength() {
-    let mdp = document.getElementById('mdp').value;
-    let isOk = mdp.length >= 6
-    
+if (mdp === '') {
+    document.getElementById('imgCheckMdp').style.display = 'none';
+    document.getElementById('imgErrorMdp').style.display = 'none';
+} else {
+   
+    document.getElementById('imgCheckMdp').style.display = 'none';
+    document.getElementById('imgErrorMdp').style.display = 'none';
+
     if (isOk) {
+        document.getElementById('imgCheckMdp').style.display = 'block';
+    } else {
+        document.getElementById('imgErrorMdp').style.display = 'block';
+    }
+}
+  }
+
+  document.getElementById('mdp').addEventListener('input', checkPwdLength)
+function checkPwdLength() {
+    
+    let mdp = document.getElementById('mdp').value;
+    let isOk= mdp.length>=6
+ 
+    if(mdp===''){
+        document.getElementById('nbCaracteres').style.color='white'
+    }
+    else if (isOk) {
         colorTextGreen('nbCaracteres')
     } else {
         colorTextRed('nbCaracteres')
     }
-    return isOk
+    
 }
 
 document.getElementById('mdp').addEventListener('input', checkNb)
 function checkNb(){
     let mdp = document.getElementById('mdp').value;
-    let isOk = /[0-9]+/.test(mdp)
+    let isOk= /[0-9]/.test(mdp)
 
-    if (isOk) {
+    if (mdp==='') {
+        document.getElementById('chiffre').style.color='white'
+    } else if(isOk) {
         colorTextGreen('chiffre')
-    } else {
+    }else{
         colorTextRed('chiffre')
     }
-return isOk
+
 }
 
 document.getElementById('mdp').addEventListener('input', checkSpecial)
 function checkSpecial(){
     let mdp = document.getElementById('mdp').value;
-    let isOk = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(mdp)
+    let isOk = /[!@#$%^&*(),.?":{}|<>]/.test(mdp)
 
-    if (isOk) {
-        colorTextGreen('caractScpecial')
+    
+    if(mdp===''){
+        document.getElementById('symbole').style.color='white'
+    }
+    
+    else if (isOk) {
+        colorTextGreen('symbole')
     } else {
-        colorTextRed('caractScpecial')
+        colorTextRed('symbole')
     }
 
-    return isOk
+    
 }
 
 
 document.getElementById('email').addEventListener('input', checkEmail)
 function checkEmail(){
-    let email = document.getElementById('email').value;
-    let isOk = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+   
+   let email = document.getElementById('email').value;
+   let isOk = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
 
-    if (isOk) {
-        colorTextGreen('emailValid')
-    } else {
-        colorTextRed('emailValid')
-    }
-    
-    return isOk
+   
+   if (email === '') {
+       document.getElementById('imgCheckEmail').style.display = 'none';
+       document.getElementById('imgErrorEmail').style.display = 'none';
+   } else {
+      
+       document.getElementById('imgCheckEmail').style.display = 'none';
+       document.getElementById('imgErrorEmail').style.display = 'none';
 
-}
+       if (isOk) {
+           document.getElementById('imgCheckEmail').style.display = 'block';
+       } else {
+           document.getElementById('imgErrorEmail').style.display = 'block';
+       }
+    }}
 
 document.getElementById('nom').addEventListener('input', checkNom)
-function checkNom(){
-    let mdp = document.getElementById('nom').value;
-    let isOk = mdp.length >= 3
-    
-    if (isOk) {
-        colorTextGreen('nomValid')
-    } else{
-        colorTextRed('nomValid')
-    }
+function checkNom() {
+    let nom = document.getElementById('nom').value;
+    let isOk = nom.length >= 3;
 
     
+    if (nom === '') {
+        document.getElementById('imgCheck').style.display = 'none';
+        document.getElementById('imgError').style.display = 'none';
+    } else {
+       
+        document.getElementById('imgCheck').style.display = 'none';
+        document.getElementById('imgError').style.display = 'none';
+
+        if (isOk) {
+            document.getElementById('imgCheck').style.display = 'block';
+        } else {
+            document.getElementById('imgError').style.display = 'block';
+        }
+}}
+
+
+document.getElementById('mdp').addEventListener('input', checkConfirmMdp)
+document.getElementById('confirmMdp').addEventListener('input', checkConfirmMdp)
+function checkConfirmMdp(){
+let mdp = document.getElementById('mdp').value;  
+    let confirmMdp = document.getElementById('confirmMdp').value;
+    
+    
+    if (confirmMdp === '') {
+        document.getElementById('imgErrorConfirmMdp').style.display = 'none';
+        document.getElementById('imgCheckConfirmMdp').style.display = 'none';
+    } else {
+        
+        if (mdp === confirmMdp) {
+            document.getElementById('imgCheckConfirmMdp').style.display = 'block';  
+            document.getElementById('imgErrorConfirmMdp').style.display = 'none';   
+        } else {
+            document.getElementById('imgCheckConfirmMdp').style.display = 'none';   
+            document.getElementById('imgErrorConfirmMdp').style.display = 'block';  
+        }
+}}
+
+
+document.getElementById('mdp').addEventListener('input', checkSecurity)
+function checkSecurity(){
+    let mdp = document.getElementById('mdp').value;
+let faible= mdp.length <6
+let moyen=mdp.length>=6 && mdp.length<9
+let fort=mdp.length>9
+
+if(mdp===''){
+    document.getElementById('faible').style.display='none'
+    document.getElementById('moyen').style.display='none'
+    document.getElementById('fort').style.display='none'
+
+}else if(faible){
+    document.getElementById('faible').style.display='block'
+}else if(moyen){
+    document.getElementById('moyen').style.display='block'
+}else if(fort){
+    document.getElementById('fort').style.display='block'
 
 }
 
-document.getElementById('confirmMdp').addEventListener('input', checkConfirmMdp)
-function checkConfirmMdp(){
-    let mdp = document.getElementById('mdp').value;
-    let confirMdp = document.getElementById('confirmMdp').value
-    if(confirMdp===mdp){
-        colorTextGreen('confirmMdpSentence')
-    }else{
-        colorTextRed('confirmMdpSentence')
-    }
 }
